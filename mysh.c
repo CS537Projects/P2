c287191428fb2e53c25e9c90a035ec3a2dd209f4
khutchins2 <khutchins2@users.noreply.github.com>
2022-02-21 19:00:24 -0600

    Update mysh.c

--- mysh.c
+++ mysh.c
@@ -2,3 +2,10 @@
 
 //Process args
 //Loop
+
+//This literally is pretty much what we need to do, 
+//we can use this annd basically go through most of the project
+//          I
+//          V
+// https://brennan.io/2015/01/16/write-a-shell-in-c/
+
