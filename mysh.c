e29205ca96495086d943b5ab4396981438c2e622
khutchins2 <khutchins2@users.noreply.github.com>
2022-02-22 18:05:41 -0600

    Added half of turtle mode

--- mysh.c
+++ mysh.c
@@ -1,7 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 //mysh.c
-
-//Process args
-//Loop
 
 //This literally is pretty much what we need to do, 
 //we can use this annd basically go through most of the project
@@ -9,3 +9,54 @@
 //          V
 // https://brennan.io/2015/01/16/write-a-shell-in-c/
 
+void turtle_mode(){
+    int exit_found = 0;
+    do{
+        const char delim[2] = " ";
+        char buf[512];
+        char *token;
+        char **array;
+        array = malloc(sizeof(char *));
+
+        printf("> ");
+        //get line
+        fgets(buf, sizeof buf, stdin);
+
+        //parse line
+        token = strtok(buf, delim);
+        int i = 0;
+        while(token != NULL) {
+            printf( "%s\n", token );
+            array[i] = malloc(sizeof(token));
+            strncpy(array[i], token, sizeof(token));
+            token = strtok(NULL, delim);
+            i++;
+        }
+        
+        //fork bs with array bs 
+        
+        //fork()bs
+        //do command
+
+        exit_found = 1;
+
+    }while(exit_found == 0);
+    exit(0);
+}
+
+int main(int argc, char **argv)
+{
+    if(argc == 1){
+        //shell mode
+        turtle_mode();
+    }else if(argc == 2){
+        //batch mode
+    }else{
+        //ERROR
+    }
+
+
+  // Perform any shutdown/cleanup.
+
+  return 0;
+}
