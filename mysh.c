59b42ecc27610b0e95e237c0460d75ad176764fc
khutchins2 <khutchins2@users.noreply.github.com>
2022-02-24 17:55:37 -0600

    Added parsing functionality

--- mysh.c
+++ mysh.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 //mysh.c
 
 //This literally is pretty much what we need to do, 
@@ -16,23 +17,36 @@ void turtle_mode(){
         char buf[512];
         char *token;
         char **array;
+
+        char ***aliasArray;
+
         array = malloc(sizeof(char *));
+        aliasArray = malloc(sizeof(char **));
 
         printf("> ");
         //get line
         fgets(buf, sizeof buf, stdin);
+        strncpy(token, buf, strlen(buf)-1);
+        strncat(token, "\0", 2);
 
-        //parse line
-        token = strtok(buf, delim);
+        token = strtok(token, delim);
         int i = 0;
         while(token != NULL) {
-            printf( "%s\n", token );
             array[i] = malloc(sizeof(token));
-            strncpy(array[i], token, sizeof(token));
+            strncpy(array[i], token, strlen(token));
             token = strtok(NULL, delim);
             i++;
         }
-        
+
+        //check array[0] for alias
+        //go through array, check for >>
+            //The exact format of redirection is: a command (along with its arguments, 
+            //if present), followed by any number of white spaces (including none), 
+            //the redirection symbol >,  again any number of white space (including none), 
+            //followed by a filename.
+        execvp(array[0], array);
+
+
         //fork bs with array bs 
         
         //fork()bs
@@ -41,7 +55,7 @@ void turtle_mode(){
         exit_found = 1;
 
     }while(exit_found == 0);
-    exit(0);
+    _exit(0);
 }
 
 int main(int argc, char **argv)
