b4fc504b003b9f8e9f99cffef51ac2160922ef59
cmdrury <61168979+cmdrury@users.noreply.github.com>
2022-02-25 18:17:25 -0600

    Bash

--- mysh.c
+++ mysh.c
@@ -12,7 +12,7 @@
 
 void turtle_mode(){
     int exit_found = 0;
-    char *temp;
+    //char *temp;
     do{
         const char delim[2] = " ";
         char buf[512];
@@ -60,6 +60,64 @@ void turtle_mode(){
     _exit(0);
 }
 
+
+void bachelorette_mode(char *file){
+    
+    
+    
+    FILE *fp = fopen(file, "r");
+        if (fp == NULL) {
+        printf("my-look: cannot open file\n");
+        _exit(1);
+    }
+    int exit = 0;
+    char buf[512];
+    while ((fgets(buf, sizeof(buf), fp) != NULL) && (exit == 0)) {
+        const char delim[2] = " ";
+        char *token = malloc(sizeof(buf));
+        char **array = malloc(sizeof(char *));
+        //fgets(buf, sizeof(buf), fp);
+        buf[strcspn(buf, "\n")] = 0;
+        token = strtok(buf, delim);
+
+        int j = 0;
+        while(token != NULL) {
+            array[j] = malloc(sizeof(token));
+            strncpy(array[j], token, strlen(token));
+            token = strtok(NULL, delim);
+            j++;
+        }
+        if(strncmp(array[0], "exit",512) == 0){
+            exit = 1;
+        }else{
+            pid_t pid = fork();
+            int status;
+            pid_t wait;
+            if(pid == 0){
+                //Child
+                execvp(array[0], array);
+                _exit(0);
+
+            }else if(pid <0){
+                //Error 
+                _exit(1);
+            }else{
+                do {
+                    wait = waitpid(pid, &status, 0);
+                } while (!WIFSIGNALED(status) && !WIFEXITED(status));
+            }
+        }
+
+        for(int i = 0; i<sizeof(array); i++){
+                array[i] = NULL;
+        }
+        free(array);
+    }
+    fclose(fp);
+
+}
+
+
 int main(int argc, char **argv)
 {
     if(argc == 1){
@@ -67,6 +125,7 @@ int main(int argc, char **argv)
         turtle_mode();
     }else if(argc == 2){
         //batch mode
+        bachelorette_mode(argv[1]);
     }else{
         //ERROR
     }
