9031a6542f45e3d7a138d7f7f4ff8f8102589203
cmdrury <61168979+cmdrury@users.noreply.github.com>
2022-03-05 16:17:09 -0600

    Combined

--- mysh.c
+++ mysh.c
@@ -5,6 +5,7 @@
 #include <fcntl.h>
 #include <sys/wait.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 int isEmpty(char* phrase){
     int length = strlen(phrase);
@@ -14,6 +15,15 @@ int isEmpty(char* phrase){
         }
     }
     return 1;
+}
+
+void Kcopy(char* from, char* to){
+    int j = 0;
+    
+    for (j = 0; from[j] != '\0'; ++j) {
+        to[j] = from[j];
+    }
+    to[j] = '\0';
 }
 
 int Ktrim(char* string){
@@ -37,32 +47,37 @@ int Ktrim(char* string){
 struct node {
     struct node* child;
     char name[512];
-    char* arg[512];
+    char **arg;
 };
 
 struct node* head = NULL;
 
-int search_alias(char *name, struct node *curr, char ***returnArray){
+bool search_alias(char *name, struct node *curr, char ***returnArray){
     if(head == NULL){
-        return 1;
+        return false;
     }
-
-    if(strcasecmp(curr->name, name) == 0){
-        return 0;
+   // printf("curr name: %s, name: %s\n", curr->name, name);
+    if(strcmp(curr->name, name) == 0){
         *returnArray = curr->arg;
+        return true;
     }
     
     if(curr-> child == NULL){
-        return 1;
+        return false;
     }
 
-    return 1;
+    return search_alias(name, curr->child, returnArray);
+
 }
 
 void store_alias(char *name, char** val){
     char** emptyArray;
     
-    if(search_alias(name,head, &emptyArray)==0){
+    if(isEmpty(name)){
+        return;
+    }
+
+    if(search_alias(name,head, &emptyArray)){
         return;
     }
 
@@ -74,17 +89,26 @@ void store_alias(char *name, char** val){
     if((strcasecmp(danger1, name)==0) || (strcasecmp(danger2, name)==0) || (strcasecmp(danger3, name)==0)){
         return;
     }
-
     struct node *addNode = (struct node*) malloc(sizeof(struct node));
+    addNode->arg = malloc(16 * sizeof(char**));
+    addNode->child = NULL;
+    
     strcpy(addNode->name, name);
-    for(int i = 2; i< sizeof(val); i++){
+    
+    int j = 0;
+    int i = 2;
+    while (val[i] != NULL) {
         //char *cpy = addNode->arg[i];
         strcpy(val[i], addNode->arg[i]);
     }
     if(head!= NULL){
         addNode->child = head->child;
     }
+    addNode->arg[j] = NULL;
+
+    struct node *old = head;
     head = addNode;
+    head->child = old;
 }
 
 int unalias(char *name, struct node *curr){
@@ -106,20 +130,70 @@ int unalias(char *name, struct node *curr){
         return 1;
     }
 
-    return 0;
+    return 1;
+}
+
+void printAlias(struct node *head){
+    while(head != NULL) {
+        printf("%s ", head->name);
+        int i =0;
+        
+        while( head->arg[i] != NULL){
+            printf(" %s", head->arg[i]);
+            i++;
+        }
+
+        printf("\n");
+        if (head->child != NULL) {
+            head = head->child;
+        } else {
+            head = NULL;
+            continue;
+        }
+    }
+}
+
+void onePrint(char *name, struct node *head){
+    if(head == NULL){
+        return;
+    }
+    
+    if(strcmp(head->name, name) == 0){
+        printf("%s ", head->name);
+        int i =0;
+        
+        while( head->arg[i] != NULL){
+            printf(" %s", head->arg[i]);
+            i++;
+        }
+
+        printf("\n");
+        return;
+    }
+    
+    // if(head-> child == NULL){
+    //     return ;
+    // }
+
+    onePrint(name, head->child);
+
+    return;
 }
 
 
 void execute(char** array){
     if(!isEmpty(array[0])){
         char** aliasArray;
-        if(search_alias(array[0], head, &aliasArray)== 0){
-                //call exec with alias
-                execv((char*)aliasArray[0], aliasArray);
-            }else{                            
-                execv(array[0], array);
-                fprintf(stderr, "%s: Command not found.\n", array[0]);  
-            }
+        bool check = search_alias(array[0], head, &aliasArray);
+        //printf("arg: %s %s\n", aliasArray[0], aliasArray[1]);
+        if(check){
+            //call exec with alias
+            execv((char*)aliasArray[0], aliasArray);
+            fprintf(stderr, "%s: Command not found.\n", array[0]);
+        }else{     
+            execv(array[0], array);
+            fprintf(stderr, "%s: Command not found.\n", array[0]);  
+        }
     }
 }
 
@@ -237,7 +311,7 @@ void turtle_mode(){
     do{
         const char delim[2] = " ";
         char buf[512];
-        char **array = malloc(64* sizeof(char **));
+        char **array = malloc(64* sizeof(char *));
         write(1, "mysh> ", 6);
         if(fgets(buf, sizeof buf, stdin) == NULL){
             exit_found = 1;
@@ -246,7 +320,7 @@ void turtle_mode(){
         
         buf[strcspn(buf, "\n")] = 0;
         int check = Ktrim(buf);
-        if(check == 1){
+        if(check == 0){
             continue;
         }
         char* token = strtok(buf, delim);
@@ -266,8 +340,12 @@ void turtle_mode(){
         }else if(strncmp(array[0], "exit",512) == 0){
             exit_found = 1;
         }else if(strncmp(array[0], "alias",512) == 0){
-            write(1, "alias detected\n", 16);
-            store_alias(array[1], array);
+            //write(1, "alias detected\n", 16);
+            if(array[1]!=NULL){
+                store_alias(array[1], array);
+            }else{
+                printAlias(head);
+            }
 
         }else if(strncmp(array[0], "unalias",512) == 0){
             write(1, "unalias detected\n", 18);
@@ -281,7 +359,8 @@ void turtle_mode(){
              free(array[i]);
         }
         free(array);
-    }while(exit_found == 0);
+        //free(token); 
+    }while(!exit_found);
     _exit(0);
 }
 
@@ -293,17 +372,20 @@ void bachelorette_mode(char *file){
         }
     int exit_found = 0;
     char buf[512];
-    while ((fgets(buf, sizeof(buf), fp) != NULL) && (exit_found == 0)){
+    while ((fgets(buf, sizeof(buf), fp) != NULL) && (!exit_found )){
+        
         const char delim[2] = " ";
         char **array = malloc(64 * sizeof(char **));
         buf[strcspn(buf, "\n")] = 0;
         int check = Ktrim(buf);
-        if(check == 1){
+        
+        if(check == 0){
             continue;
         }
 
         char *token = strtok(buf, delim);
         int length = 0;
+        
         while(token != NULL) {
             if (!isEmpty(token)) {
                 array[length] = malloc(strlen(token) + 1);
@@ -322,6 +404,8 @@ void bachelorette_mode(char *file){
                 write(1, "\n", 1);
             }
         }
+
+       
         if(isEmpty(array[0])){
             continue;
         }
@@ -330,8 +414,15 @@ void bachelorette_mode(char *file){
             exit_found = 1;
         }else if(strncmp(array[0], "alias",512) == 0){
             //Is array[2] catching all the arguments
-            store_alias(array[1], array);
-            write(1, "alias detected\n", 16);
+            if(array[2]!=NULL){
+                store_alias(array[1], array);
+            }else if(array[1]!=NULL){
+                onePrint(array[1], head);
+            }else{
+                printAlias(head);
+            }
+            
+            //write(1, "alias detected\n", 16);
         }else if(strncmp(array[0], "unalias",512) == 0){
             unalias(array[1], head);
             write(1, "unalias detected\n", 18);
